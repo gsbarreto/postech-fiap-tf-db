1af4ee37046b3ba7d859ce2be02396f1ae9fe118
Gabriel Barreto <gsbarretobr@gmail.com>
2024-03-18 18:57:28 -0300

    feat: changed user role

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "mongodbatlas_database_user" "db-user" {
   project_id = mongodbatlas_project.atlas-project.id
   auth_database_name = "admin"
   roles {
-    role_name     = "readWrite"
+    role_name     = "readWriteAnyDatabase"
     database_name = "${var.project_name}-db"
   }
 }
